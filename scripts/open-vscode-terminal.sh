16bcd7d15936622af553c1d5ff53231fc25c40c5
inamuu <8310973+inamuu@users.noreply.github.com>
2023-01-10 11:37:32 +0900

    feat: add a open vscode with null text

---
+++ scripts/open-vscode-terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open-vscode-terminal
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+cd ${HOME}/Downloads
+code ${HOME}/Downloads/$(date +%Y%m%d-%H%M%S).txt
