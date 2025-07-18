1233607b4675ef2ad24d33100a472e9caf28f4be
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-07-18 12:58:44 +0900

    fix region

--- scripts/open_aws_console.sh
+++ scripts/open_aws_console.sh
@@ -5,7 +5,7 @@
 # @raycast.title Open Aws Console
 # @raycast.mode compact
 # @raycast.needsConfirmation false
-# @raycast.argument1 { "type": "dropdown", "placeholder": "region", "data" : [{"title": "tokyo", "value": "ap-northeast-1"}] }
+# @raycast.argument1 { "type": "dropdown", "placeholder": "region", "data" : [{"title": "tokyo", "value": "ap-northeast-1"}, {"title": "us-east", "value": "us-east-1"}] }
 # @raycast.argument2 { "type": "dropdown", "placeholder": "services", "data" : [{"title": "cloudfront", "value": "cloudfront/v4/home#/distributions"},{"title": "cloudwatch", "value": "cloudwatch/home"},{"title": "ec2", "value": "ec2/home"},{"title": "ec2/SecurityGroups", "value": "ec2/home#SecurityGroups:"},{"title": "ec2/LoadBalancers", "value": "ec2/home#LoadBalancers:"},{"title": "ecr", "value": "ecr/home"},{"title": "ecs", "value": "ecs/v2/clusters"},{"title": "iam", "value": "iam/home"},{"title": "iam/roles", "value": "iam/home#/roles"},{"title": "iam/policies", "value": "iam/home#/policies"},{"title": "rds", "value": "rds/home"},{"title": "s3", "value": "s3/home"},{"title": "ssm/parameters", "value": "systems-manager/parameters"},{"title": "route53", "value": "route53/v2/home"},{"title": "vpc", "value": "vpcconsole/home"},{"title": "waf", "value": "wafv2/homev2/home"}]}
 
 # Optional parameters:
